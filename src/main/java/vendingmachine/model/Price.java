2732ceb27cf0933e57a6c3b9d50549f5c5922b3a
HyeonsuLee <leehyeonsu4888@naver.com>
2024-12-01 13:59:09 +0900

    feat: 가격 Model 작성

---
+++ src/main/java/vendingmachine/model/Price.java
@@ -0,0 +1,18 @@
+package vendingmachine.model;
+
+import vendingmachine.exception.ProductPriceFromatException;
+
+public class Price {
+    private final int price;
+
+    public Price(int price) {
+        validateAmount(price);
+        this.price = price;
+    }
+
+    private void validateAmount(int price) {
+        if (price < 100 || price % 10 != 0) {
+            throw new ProductPriceFromatException();
+        }
+    }
+}
