3cfdc33852b111699b969ba187ba299ab310d06b
HyeonsuLee <leehyeonsu4888@naver.com>
2024-12-01 16:27:31 +0900

    feat: 자판기 금액 model작성

---
+++ src/main/java/vendingmachine/model/MachineAmount.java
@@ -0,0 +1,26 @@
+package vendingmachine.model;
+
+import vendingmachine.exception.AmountFormatException;
+
+public class MachineAmount {
+    private int amount;
+
+    public MachineAmount(int amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < 0 || amount % 10 != 0) {
+            throw new AmountFormatException();
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void decrease(int amount) {
+        this.amount -= amount;
+    }
+}
